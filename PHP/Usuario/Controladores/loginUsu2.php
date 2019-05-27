a4bec21c51eb60fb30a414ca0577aedf1b363e8d
Christian Garcia <christian.garciap@hotmail.com>
2019-05-27 18:23:31 -0500

    Inicio sesiones usuarios, empresa

--- PHP/Usuario/Controladores/loginUsu2.php
+++ PHP/Usuario/Controladores/loginUsu2.php
@@ -7,6 +7,7 @@ include '../../Conexion/conexionBD.php';
  
     $user = "SELECT * FROM usuario WHERE usu_rol='USUARIO' AND usu_correo = '$usuario' and usu_password = MD5('$contrasena')"; 
     $admin = "SELECT * FROM usuario WHERE usu_rol='ADMINISTRADOR' AND usu_correo = '$usuario' and usu_password = MD5('$contrasena')"; 
+    $empresa = "SELECT * FROM empresa WHERE emp_correo='$usuario' and emp_password = MD5('$contrasena')";
 
     $result1 = $conn->query($user);            
     if ($result1->num_rows > 0) 
@@ -40,7 +41,24 @@ include '../../Conexion/conexionBD.php';
     {             
         header("Location: ../loginUsu.html"); 
        
-    }              
+    }   
+    
+    $result3 = $conn->query($empresa);            
+    if ($result3->num_rows > 0) 
+    {
+        while($row = $result3->fetch_assoc()) 
+        {                   
+            session_start(); 
+            $_SESSION['isLogged']  = TRUE;         
+            header("Location: ../../Empresa/index_empresa.php?codigo=". $row['usu_codigo'] . '&usuario='.$row['usu_correo'].""); 
+            
+            exit();    
+        }
+    } else 
+    {             
+        header("Location: loginUsu.php"); 
+       
+    }   
    
 ?> 
  

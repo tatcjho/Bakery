e1b6fb62d79a1dda105777342bc6dce94c905dbd
Christian Garcia <christian.garciap@hotmail.com>
2019-05-27 18:06:26 -0500

    redireccionamiento

--- PHP/Empresa/cabecera.php
+++ PHP/Empresa/cabecera.php
@@ -39,9 +39,9 @@
                 <li class="submenu">
                     <a href="#">Productos <span class="fa fa-caret-down"></span></a>
                     <ul class="children">
-                        <li><a href="pan.php">Panes</a></li>
-                        <li><a href="cookies.php">Galletas</a></li>
-                        <li><a href="cake.php">Tortas</a></li>
+                        <li><a href="../pan.php">Panes</a></li>
+                        <li><a href="../cookies.php">Galletas</a></li>
+                        <li><a href="../cake.php">Tortas</a></li>
                         <li> <a href="#">Cupcake</a></li>
                     </ul>
                 </li>

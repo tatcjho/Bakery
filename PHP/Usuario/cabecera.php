a2c4927d82a80efe7cb915386ac149b6c786d58a
Mauricio <mauricio@MacBook-Pro-de-Mauricio.local>
2019-05-30 11:46:48 -0500

    datos editados

--- PHP/Usuario/cabecera.php
+++ PHP/Usuario/cabecera.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="../../CSS/indexEmpresa.css">
     <script src="http://code.jquery.com/jquery-latest.js"></script>
-    <script src="main.js"></script>
+    <script src="../main.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
--- PHP/Usuario/editarPerfil.php
+++ PHP/Usuario/editarPerfil.php
@@ -1,13 +1,14 @@
 <?php
-    session_start();
-    if(!isset($_SESSION['isLogged']) || $_SESSION['isLogged']==false){
-        header("Location: ../login.php");
-    }else if($_SESSION['usu_rol'] == "A"){
-        header("Location: ../Administrador/index_administrador.php");
-    }
+session_start();
+if (!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] == false) {
+    header("Location: ../login.php");
+} else if ($_SESSION['usu_rol'] == "A") {
+    header("Location: ../Administrador/index_administrador.php");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scaleble=no, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0">
@@ -15,14 +16,19 @@
     <title>Inicio</title>
     <link rel="stylesheet" href="../../CSS/editarPerfil.css">
 </head>
+
 <body>
     <div class="cabecera">
         <?php
         include("cabecera.php");
         ?>
     </div>
+
+
     <div id="navIzq">
+
         <ul>
+
             <li><a href="editarPerfil.php" class="active">Modificar Cuenta</a></li>
             <li><a href="editarContrasena.php">Cambiar Contraseña</a></li>
             <li><a href="#contact" class="eliminar">Eliminar Cuenta</a></li>
@@ -31,40 +37,54 @@
     <div id="derecha">
         <span id="titulo">Modificacion de Cuenta</span>
         <div id="contenedor">
-        <?php
-        $codigo=$_SESSION['usu_codigo'];
-        $sql="SELECT * FROM usuario WHERE usu_codigo=$codigo";
-        include '../Conexion/conexionBD.php'; 
-        $result=$conn->query($sql);
-        if($result->num_rows > 0){
-            while($row=$result->fetch_assoc()){
-                ?>
-                <form id="formulario01" method="post" action="Controladores/modificarUsuario.php">
-                    <input type="hidden" id="codigo" name="codigo" value="<?php echo $codigo ?>"/>
-                    <label for="cedula" id="label1">Cedula</label>
-                    <input type="text" id="cedula" name="cedula" value="<?php echo $row["usu_cedula"]; ?>" required placeholder="Ingresar cedula"/>
-                    <br>
-                    <label for="nombres" id="label1">Nombres</label>
-                    <input type="text" id="nombres" name="nombres" style="text-transform:uppercase"  value="<?php echo $row["usu_nombreApellido"]; ?>" required placeholder="Ingresar nombres"/>
-                    <br>
-                    <label for="apellidos" id="label1">Direccion</label>
-                    <input type="text" id="direccion" name="direccion" style="text-transform:uppercase" value="<?php echo $row["usu_direccion"]; ?>" required placeholder="Ingresar direccion"/>
-                    <br>
-                    <label for="telefono" id="label1">Teléfono</label>
-                    <input type="text" id="telefono" name="telefono" value="<?php echo $row["usu_telefono"]; ?>" required placeholder="Ingrese el teléfono"/>
-                    <br>
-                    <label for="fecha" id="label1">Fecha Nacimiento</label>
-                    <input type="date" id="fechaNacimiento" name="fechaNacimiento" value="<?php echo $row["usu_fecha_nacimiento"]; ?>" required placeholder="Ingrese la fecha de nacimiento"/>
-                    <br>
-                    <label for="correo" id="label1">Correo electrónico</label>
-                    <input type="email" id="correo" name="correo" value="<?php echo $row["usu_correo"]; ?>" required placeholder="Ingrese el correo electrónico"/>
-                    <br>
-                    <input type="submit" id="submit" name="modificar" value="Modificar"/>
-                    <input type="reset" id="cancelar" name="cancelar" value="Cancelar"/>
-                </form>
-                <?php   
+
+
+
+            <?php
+            $codigo = $_SESSION['usu_codigo'];
+            $sql = "SELECT * FROM usuario WHERE usu_codigo=$codigo";
+            include '../Conexion/conexionBD.php';
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    ?>
+                    <form id="formulario01" method="post" action="Controladores/modificarUsuario.php">
+                        <input type="hidden" id="codigo" name="codigo" value="<?php echo $codigo ?>" />
+                        <label for="cedula" id="label1">Cedula</label>
+                        <input type="text" id="cedula" name="cedula" value="<?php echo $row["usu_cedula"]; ?>" required placeholder="Ingresar cedula" />
+                        <br>
+                        <label for="nombres" id="label1">Nombres</label>
+                        <input type="text" id="nombres" name="nombres" style="text-transform:uppercase" value="<?php echo $row["usu_nombreApellido"]; ?>" required placeholder="Ingresar nombres" />
+                        <br>
+                        <label for="apellidos" id="label1">Direccion</label>
+                        <input type="text" id="direccion" name="direccion" style="text-transform:uppercase" value="<?php echo $row["usu_direccion"]; ?>" required placeholder="Ingresar direccion" />
+                        <br>
+                        <label for="telefono" id="label1">Teléfono</label>
+                        <input type="text" id="telefono" name="telefono" value="<?php echo $row["usu_telefono"]; ?>" required placeholder="Ingrese el teléfono" />
+                        <br>
+                        <label for="fecha" id="label1">Fecha Nacimiento</label>
+                        <input type="date" id="fechaNacimiento" name="fechaNacimiento" value="<?php echo $row["usu_fecha_nacimiento"]; ?>" required placeholder="Ingrese la fecha de nacimiento" />
+                        <br>
+                        <label for="correo" id="label1">Correo electrónico</label>
+                        <input type="email" id="correo" name="correo" value="<?php echo $row["usu_correo"]; ?>" required placeholder="Ingrese el correo electrónico" />
+
+                        <div class="foto">
+
+
+                            <img src="../../images<?php echo ($row["usu_imagen"]) ?>" alt="">
+                            <br>
+                            <br>
+                            <input type="file" name="foto" id="foto">
+
+                        </div>
+
+                        <br>
+                        <input type="submit" id="submit" name="modificar" value="Modificar" />
+                        <input type="reset" id="cancelar" name="cancelar" value="Cancelar" />
+                    </form>
+                <?php
             }
-        }else {
+        } else {
             echo "<p>Ha ocurrido un error inesperado</p>";
             echo "<p>" . mysqli_error($conn) . "</p>";
         }
@@ -73,4 +93,5 @@
         </div>
     </div>
 </body>
+
 </html>

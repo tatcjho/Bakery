df19d15d88abd2f0c13cfa3797c5003b0c3316ad
Sterling01 <stalinsant1@gmail.com>
2019-06-02 21:19:14 -0500

    Update conexionBD.php

--- PHP/Conexion/conexionBD.php
+++ PHP/Conexion/conexionBD.php
@@ -1,5 +1,5 @@
 ﻿<?php          
-$db_servername = "localhost";     
+$db_servername = "35.198.6.124";     
 $db_username = "root";     
 $db_password = ""; 
 $db_name = "pasteleria";                  
